df523ff5beaf250adbd93c3764b8703ea3b93a41
Koni106 <75367345+Koni106@users.noreply.github.com>
2020-12-03 18:58:31 +0100

    Character
    Started working on player character.

---
+++ Prototype/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControl : MonoBehaviour
+{
+    public CharacterController CC;
+    public Controls Controls;
+
+    [Tooltip("Units/seconds")]
+    [Range(0,30)]
+    public float Speed = 10;
+
+    [Tooltip("How smooth the character turns")]
+    [Range(0.0f,1f)]
+    public float TurnSmoothTime = .1f;
+
+    private Vector3 _velocity = Vector3.zero;
+    private Vector2 _movementInput;
+    private float _forwardInput, _horizontalInput,_turnSmoothVelocity;
+
+    public float JumpHeight;
+    private bool _jump;
+    private void Awake()
+    {
+        Controls = new Controls();
+        Controls.PlayerControls.Jump.performed += context => _jump = true;
+        Controls.PlayerControls.Jump.canceled += context => _jump = false;
+
+        Controls.PlayerControls.Move.performed += context => _movementInput = context.ReadValue<Vector2>();
+        Controls.PlayerControls.Move.canceled += context => _movementInput = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        _forwardInput = _movementInput.y;
+        _horizontalInput = _movementInput.x;       
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyGravity();
+        ApplyMovement();        
+
+        if (CC.isGrounded)
+        {
+            _velocity.y = -CC.stepOffset * 10;
+        }
+        ApplyJump();
+        CC.Move(_velocity * Time.fixedDeltaTime);
+    }
+
+    private void ApplyGravity()
+    {
+        if (!CC.isGrounded)
+        {
+            _velocity.y += Physics.gravity.y * Time.fixedDeltaTime;
+        }
+       
+    }   
+
+    private void ApplyMovement()
+    {
+        float horizontal = _horizontalInput;
+        float vertical = _forwardInput;
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if(direction.magnitude >= .1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, TurnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+            CC.Move(direction * Speed * Time.deltaTime);
+        }
+    }
+
+    private void ApplyJump()
+    {        
+        if (_jump && CC.isGrounded)
+        {
+            _jump = false;
+            _velocity.y = Mathf.Sqrt(2 * Mathf.Abs(Physics.gravity.y) * JumpHeight);
+        }
+    }   
+
+    private void OnEnable()
+    {
+        Controls.PlayerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        Controls.PlayerControls.Disable();
+    }
+}
---
+++ Prototype/Assets/Scripts/Controls.cs
@@ -0,0 +1,161 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerControls"",
+            ""id"": ""131984d3-3908-4f59-8602-abc8d2484373"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""1dbd94be-a4f2-4562-9ff3-a0d24f3736ba"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""f23c743b-44dc-42e5-8d6f-6d1f0619d362"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""b229175f-73bb-42ea-b533-d9061d2151c0"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f32df31b-a7be-4d12-8f74-de79201c36f8"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerControls
+        m_PlayerControls = asset.FindActionMap("PlayerControls", throwIfNotFound: true);
+        m_PlayerControls_Move = m_PlayerControls.FindAction("Move", throwIfNotFound: true);
+        m_PlayerControls_Jump = m_PlayerControls.FindAction("Jump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerControls
+    private readonly InputActionMap m_PlayerControls;
+    private IPlayerControlsActions m_PlayerControlsActionsCallbackInterface;
+    private readonly InputAction m_PlayerControls_Move;
+    private readonly InputAction m_PlayerControls_Jump;
+    public struct PlayerControlsActions
+    {
+        private @Controls m_Wrapper;
+        public PlayerControlsActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_PlayerControls_Move;
+        public InputAction @Jump => m_Wrapper.m_PlayerControls_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerControlsActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerControlsActions instance)
+        {
+            if (m_Wrapper.m_PlayerControlsActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerControlsActions @PlayerControls => new PlayerControlsActions(this);
+    public interface IPlayerControlsActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
