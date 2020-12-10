2b8b0c15441511b53ebfb4b76b638dd9715f5772
Lowskii <Lowskii@noreply.github.com>
2020-12-10 21:10:02 +0100

    Character selection screen
    Move characters from one scene to the other while keeping controllers correctly. Added the scenes to the manager in order to use the scenemanager code.

---
+++ Prototype/Assets/Scripts/GameSceneBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync("CharacterSelectionScreen");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
