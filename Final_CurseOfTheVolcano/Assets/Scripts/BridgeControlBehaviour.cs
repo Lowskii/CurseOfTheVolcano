7541e4025a4f91f62bcaf23f876fe4575ef7e6ad
Koni106 <75367345+Koni106@users.noreply.github.com>
2020-12-22 15:09:27 +0100

    Bridge+SpinningPlatform

---
+++ Final_CurseOfTheVolcano/Assets/Scripts/BridgeControlBehaviour.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeControlBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Bridge;
+    [SerializeField] private bool m_IsBridgeActivated;
+    [SerializeField] private float m_ResetDelay;
+
+    private float m_DelayTimer;
+    private Vector3 m_StartPosition;
+    private Quaternion m_StartRotation;
+
+
+    void Start()
+    {
+        m_StartPosition = m_Bridge.transform.position;
+        m_StartRotation = m_Bridge.transform.rotation;
+    }
+    
+    void Update()
+    {
+        if (m_IsBridgeActivated) DropBridge();
+        else ResetBridge();
+    }
+
+    private void DropBridge()
+    {
+        var rB = m_Bridge.GetComponent<Rigidbody>();
+        if (!m_IsBridgeActivated)
+        {
+            rB.isKinematic = true;
+            return;
+        }
+
+        rB.isKinematic = false;
+        StartDelayTimer();
+    }
+
+    private void ResetBridge()
+    {
+        var rB = m_Bridge.GetComponent<Rigidbody>();
+        rB.velocity = Vector3.zero;
+        m_Bridge.transform.position = m_StartPosition;
+        m_Bridge.transform.rotation = m_StartRotation;
+    }
+
+    private void StartDelayTimer()
+    {
+        if (m_IsBridgeActivated)
+        {
+            m_DelayTimer += Time.deltaTime;
+            if (m_DelayTimer > m_ResetDelay)
+            {
+                m_IsBridgeActivated = false;
+                m_DelayTimer -= m_ResetDelay;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if (other.gameObject.GetComponent<CharacterControl>() != null)
+        //{
+        //    _player = other.gameObject.GetComponent<CharacterControl>();
+        //    if (_player.Interact)
+        //    {
+        //        m_IsBridgeActivated = true;
+        //    }
+        //}
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //if (_player != null)
+        //{
+        //    _player.Interact = false;
+        //    _player = null;
+        //}
+    }
+}
---
+++ Final_CurseOfTheVolcano/Assets/Scripts/SpinningPlatformBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningPlatformBehaviour : MonoBehaviour
+{
+    [SerializeField] private float m_TurnSpeed;
+    [SerializeField] private bool m_IsSpinning;
+  
+    void Update()
+    {
+        if (m_IsSpinning) this.transform.Rotate(Vector3.up * m_TurnSpeed * Time.deltaTime);
+    }
+
+    public void ActivateSpinning()
+    {
+        m_IsSpinning = true;
+    }
+
+    public void DeactivateSpinning()
+    {
+        m_IsSpinning = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = this.transform;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
