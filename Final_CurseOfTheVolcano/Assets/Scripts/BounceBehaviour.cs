a3755d10c640ff7e17f218e631c0a84310dbc77c
Koni106 <75367345+Koni106@users.noreply.github.com>
2020-12-25 16:36:03 +0100

    Obstacle+Menu

---
+++ Final_CurseOfTheVolcano/Assets/Scripts/BounceBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 m_BounceForce;
+    private Transform m_Player;
+    private bool m_IsPushing;
+
+    private void Update()
+    {
+        PushPlayers();
+    }
+    private void PushPlayers()
+    {
+        if (m_Player != null && m_IsPushing)
+        {
+            StartCoroutine(PushBack());
+            m_Player.transform.position += m_BounceForce * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GetComponent<PlatformMovement>().IsMoving)
+        {
+            m_Player = other.gameObject.transform;
+            m_IsPushing = true;
+        }
+    }
+    IEnumerator PushBack()
+    {
+        while (m_IsPushing)
+        {
+            yield return new WaitForSeconds(.1f);
+            m_Player = null;
+            m_IsPushing = false;
+        }
+    }
+}
---
+++ Final_CurseOfTheVolcano/Assets/Scripts/TrampolineBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineBehaviour : MonoBehaviour
+{
+    [SerializeField] private float m_TrampolineForce;
+    private float m_JumpHeight;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterControl>() != null)
+        {
+            //Start Player Jump
+            //m_JumpHeight = other.gameObject.GetComponent<CharacterControl>().JumpHeight;
+            //other.gameObject.GetComponent<CharacterControl>().JumpHeight = m_TrampolineForce;
+            //other.gameObject.GetComponent<CharacterControl>().StartJump();
+        }
+        else
+            return;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterControl>() != null)
+        {
+           // other.gameObject.GetComponent<CharacterControl>().JumpHeight = m_JumpHeight;
+        }
+    }
+}
