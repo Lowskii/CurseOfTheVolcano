52837cfb0e7ec0d48731c07e2f889bd0f622f674
Lowskii <Lowskii@noreply.github.com>
2020-12-29 23:18:59 +0100

    push and save
    Redid push to make it work and added the ability to save yourself when falling of the map with a singlejump

--- Final_CurseOfTheVolcano/Assets/Scripts/CharacterControl.cs
+++ Final_CurseOfTheVolcano/Assets/Scripts/CharacterControl.cs
@@ -13,15 +13,12 @@ public class CharacterControl : MonoBehaviour
     [Range(0, 30)]
     public float m_MovementSpeed;
 
-    [Range(0, 100)]
-    public float NormalPushForce;
-    [Range(0, 200)]
-    public float StrongPushForce;
-    private float CurrentPushForce;
-    private float m_TurnSmoothVelocity;
-
+    [Range(0, 3)]
+    public float m_NormalPushForce;
     [Range(0, 5)]
-    public float JumpHeight;
+    public float m_StrongPushForce;
+    private float m_CurrentPushForce;
+    private float m_TurnSmoothVelocity;
 
     [Tooltip("How smooth the character turns")]
     [Range(0, 1)]
@@ -39,52 +36,55 @@ public class CharacterControl : MonoBehaviour
 
     public Vector3 m_MoveDirection = Vector3.zero;
 
-    private bool m_IsPushPossible;
-    public bool m_IsPushActivated;
+    //pushing
+    private bool m_IsPushActivated = false;
+    private bool m_IsPushPossible = false;
+    private float m_PushTimer = 0f;
+    private float m_PushedSlowFactor = 3f;
+    private float m_KnockBackTimer = 1f;
+    [SerializeField] float m_PushDelay = 2.5f;
+    bool m_GettingPushed = false;
 
-    private float m_PushTimer,m_DelayTimer;
+    //saving
+    bool m_IsSaveJumpAvailable = false;
+    float m_SaveTime = 1.6f;
+
+    private float m_DelayTimer;
     private Vector3 m_Inpact = Vector3.zero;
+    private Vector3 m_CurrentInpact = Vector3.zero;
 
 
     private float m_VerticalInput, m_HorizontalInput;
-    public bool m_IsMovementInversed=true;
-    public  bool m_IsSpeedDown=false;
+    public bool m_IsMovementInversed = true;
+    public bool m_IsSpeedDown = false;
     public bool m_IsBouncing = false;
     public bool m_Paralyse = false;
 
 
-    private bool m_HasMoved = false;
     private bool m_IsDoubleJumpPossible = false;
     private bool m_IsDoubleJumpEnabled = false;
-    public bool IsInteractPressed = false;    
+    public bool IsInteractPressed = false;
 
 
-
+    private void Awake()
+    {
+        m_CurrentPushForce = m_NormalPushForce;
+    }
     private void Update()
     {
         ApplyMovement();
         ApplyPush();
-        StartDelayTimer();        
+        StartDelayTimer();
+        ApplyKnockBack();
     }
 
-    private void ApplyPush()
+    private void FixedUpdate()
     {
-        if (m_IsPushActivated)
-        {
-            m_PushTimer += Time.deltaTime;
-            ApplyKNockBack();
-            if (m_PushTimer >= 3 == false)
-            {
-                m_IsPushActivated = false;
-                m_PushTimer = 0;
-            }
-        }
-    }
 
+    }
 
     private void ApplyMovement()
     {
-        if(!m_HasMoved) return;
 
         if (m_CharacterController.isGrounded)
         {
@@ -97,7 +97,6 @@ public class CharacterControl : MonoBehaviour
             {
                 m_IsDoubleJumpPossible = true;
             }
-
         }
         else
         {
@@ -115,23 +114,41 @@ public class CharacterControl : MonoBehaviour
 
         Vector3 velocity = m_MoveDirection;
 
+        //while getting pushed your movement is very limited
+        if (m_GettingPushed)
+        {
+            velocity.x /= m_PushedSlowFactor;
+            velocity.z /= m_PushedSlowFactor;
+        }
+
         if (m_IsMovementInversed)
         {
             velocity.x = -velocity.x;
             velocity.z = -velocity.z;
         }
+
         if (m_IsSpeedDown)
         {
             m_MovementSpeed = m_MovementSpeed / 2;
         }
-        m_CharacterController.Move(velocity * Time.deltaTime * m_MovementSpeed);
+
+
+        if (velocity.magnitude > 0.1f)
+        {
+            m_CharacterController.Move(velocity * Time.deltaTime * m_MovementSpeed);
+        }
     }
 
     public void Jump(InputAction.CallbackContext value)
     {
-        if (m_CharacterController.isGrounded || (m_IsDoubleJumpPossible && m_IsDoubleJumpEnabled))
+        if (m_CharacterController.isGrounded || (m_IsDoubleJumpPossible && m_IsDoubleJumpEnabled) || m_IsSaveJumpAvailable)
         {
             m_MoveDirection.y = m_Jumpspeed;
+
+            if (!m_CharacterController.isGrounded)
+            {
+                m_IsSaveJumpAvailable = false;
+            }
             if (m_IsDoubleJumpPossible && !m_CharacterController.isGrounded)
             {
                 m_IsDoubleJumpPossible = false;
@@ -141,8 +158,6 @@ public class CharacterControl : MonoBehaviour
 
     public void Movement(InputAction.CallbackContext value)
     {
-        m_HasMoved = true;
-
         var movement = value.ReadValue<Vector2>();
 
         m_HorizontalInput = movement.x;
@@ -154,25 +169,6 @@ public class CharacterControl : MonoBehaviour
         m_MoveDirection.Normalize();
         m_MoveDirection.y = yDir;
     }
-    public void Push(InputAction.CallbackContext value)
-    {
-        m_IsPushActivated = true;
-        m_IsPushPossible = true;
-    }
-
-    public void KnockBack(Vector3 Direction, float Force)
-    {
-        Direction.Normalize();
-        if (Direction.y < 0) Direction.y = -Direction.y; // reflect down force on the ground
-        m_Inpact += Direction.normalized * Force / m_Mass;
-
-    }
-    public void ApplyKNockBack()
-    {
-        if (m_Inpact.magnitude > 0.2) m_CharacterController.Move(m_Inpact * Time.deltaTime * CurrentPushForce);
-        // consumes the impact energy each cycle:
-        m_Inpact = Vector3.Lerp(transform.position,transform.position + m_Inpact, 5 * Time.deltaTime);
-    }
 
     public void Interact(InputAction.CallbackContext value)
     {
@@ -180,9 +176,9 @@ public class CharacterControl : MonoBehaviour
     }
 
     private void StartDelayTimer()
-    {       
+    {
         if (IsInteractPressed)
-        {            
+        {
             float resetDelay = .1f;
             m_DelayTimer += Time.deltaTime;
             if (m_DelayTimer > resetDelay)
@@ -192,24 +188,87 @@ public class CharacterControl : MonoBehaviour
             }
         }
     }
-    
+
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.GetMask("Player")&& m_IsPushPossible)
-        {
-            GameObject Player = other.gameObject;
-            Player.GetComponent<CharacterControl>().KnockBack(m_MoveDirection, CurrentPushForce);
-            Player.GetComponent<CharacterControl>().m_IsPushActivated = true;
-            m_IsPushPossible = false;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.layer == LayerMask.GetMask("Player"))
+        if (other.gameObject == gameObject) return;
+
+        if (other.gameObject.tag == "Player" && m_IsPushActivated && m_IsPushPossible)
         {
             m_IsPushPossible = false;
 
+            GameObject Player = other.gameObject;
+            Vector3 dir = Player.transform.position - transform.position;
+
+            Player.GetComponent<CharacterControl>().KnockBack(dir);
+        }
+        m_IsPushActivated = false;
+    }
+
+    public void Push(InputAction.CallbackContext value)
+    {
+        m_IsPushActivated = true;
+
+        Invoke("DisablePushActivated", 0.25f);
+    }
+
+    public void KnockBack(Vector3 direction)
+    {
+        direction.y = 0; //we don't want people getting knocked up or down
+        direction.Normalize();
+
+        m_Inpact = direction * m_NormalPushForce;
+        m_CurrentInpact = m_Inpact;
+
+        m_KnockBackTimer = 0f;
+        m_GettingPushed = true;
+
+        m_IsSaveJumpAvailable = true;
+        Invoke("DisableSaveJump", m_SaveTime);
+    }
+    public void ApplyKnockBack()
+    {
+        if (m_KnockBackTimer < 1f)
+        {
+            m_KnockBackTimer += Time.deltaTime;
+
+            //get the new position of this frame and reduce the distance of where the player will be trown
+            Vector3 destPos = Vector3.Lerp(transform.position, transform.position + m_CurrentInpact, m_KnockBackTimer);
+            m_CurrentInpact = Vector3.Lerp(m_Inpact, Vector3.zero, m_KnockBackTimer);
+
+            Vector3 motion = destPos - transform.position;
+
+            if (motion.magnitude > 0.1f)
+            {
+                m_CharacterController.Move(motion);
+            }
+        }
+        else
+        {
+            m_GettingPushed = false;
         }
     }
- 
+
+    private void ApplyPush()
+    {
+        if (!m_IsPushPossible)
+        {
+            m_PushTimer += Time.deltaTime;
+
+            if (m_PushTimer >= m_PushDelay)
+            {
+                m_IsPushPossible = true;
+                m_PushTimer = 0;
+            }
+        }
+    }
+
+    void DisablePushActivated()
+    {
+        m_IsPushActivated = false;
+    }
+    void DisableSaveJump()
+    {
+        m_IsSaveJumpAvailable = false;
+    }
 }
